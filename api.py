1e5e4e9fb8963e0a5a1a3735f4d86ec1117b7bbb
Aaryan Kamalwanshi <162993299+Aaryank-47@users.noreply.github.com>
2026-03-26 17:24:46 +0530

    added headless chrome config for render

--- api.py
+++ api.py
@@ -234,7 +234,9 @@ def get_driver():
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument(f"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{random.randint(100,120)}.0.0.0 Safari/537.36")
     options.add_argument("--window-size=1920,1080")
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     
     driver = uc.Chrome(use_subprocess=True, options=options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
